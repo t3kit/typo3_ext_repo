76796cb3ad163e6cba05b45e0e40b4bdaf9bca97
dmh <dmytro.hrynevych@gmail.com>
2016-05-16 18:30:34 +0300

    initial commit

---
+++ pack.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Simple bash script to zip some extensions for TYPO3 extension repository
+# https://typo3.org/extensions/repository/
+
+
+# theme_t3kit
+git clone git@github.com:t3kit/theme_t3kit.git
+cd theme_t3kit
+git submodule update --init --recursive
+git checkout -b release
+LAST_TAG1=`git describe --abbrev=0 --tags`
+echo "theme_t3kit" $LAST_TAG1
+cd Resources/Public/felayout_t3kit
+git archive -o ../../../felayout_t3kit.zip HEAD
+cd -
+git rm Resources/Public/felayout_t3kit
+unzip felayout_t3kit.zip -d Resources/Public/felayout_t3kit
+rm felayout_t3kit.zip
+git add .
+git commit -am 'release'
+git archive -o ../"${PWD##*/}_$LAST_TAG1.zip" HEAD
+cd ..
+rm -rf theme_t3kit
+
+
+# t3kit_extension_tools
+git clone git@github.com:t3kit/t3kit_extension_tools.git
+cd t3kit_extension_tools
+LAST_TAG2=`git describe --abbrev=0 --tags`
+echo "t3kit_extension_tools" $LAST_TAG2
+git archive -o ../"${PWD##*/}_$LAST_TAG2.zip" HEAD
+cd ..
+rm -rf t3kit_extension_tools
